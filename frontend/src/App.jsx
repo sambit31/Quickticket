d8d5323f2b9cc7965d1622b454302c88ba03c685
Sambit Ghosh <sambitg31@gmail.com>
2026-06-24 05:29:29 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import MyBooking from './pages/MyBooking'
+import Favorite from './pages/Favorite'
+
+import{Toaster} from 'react-hot-toast'
+import Footer from './components/Footer'
+
+const App = () => {
+
+  const isAdminRoute = window.location.pathname.startsWith('/admin');
+
+  return (
+    <>
+    <Toaster />
+    {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBooking />} />
+        <Route path="/favorites" element={<Favorite />} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
+  )
+}
+
+export default App
