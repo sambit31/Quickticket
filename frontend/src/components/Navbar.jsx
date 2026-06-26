4f135ce3514c6e77f1cda93ea0ef583141f678de
Sambit Ghosh <sambitg31@gmail.com>
2026-06-26 22:46:23 +0530

    clerk introduced

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets.js'
-import { MenuIcon, XIcon, SearchIcon } from 'lucide-react'
+import { MenuIcon, XIcon, SearchIcon, TicketPlus } from 'lucide-react'
+import { useClerk, UserButton, useUser } from '@clerk/react'
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const { user } = useUser()
+  const { openSignIn } = useClerk()
+  const navigate = useNavigate()
 
   const handleLinkClick = () => {
     window.scrollTo(0, 0)
@@ -19,45 +24,45 @@ const Navbar = () => {
       </Link>
 
       <div
-        className={`max-md:absolute max-md:top-0 z-50 max-md:w-full max-md:h-screen max-md:px-9 max-md:font-medium max-md:text-lg flex flex-col md:flex-row items-center max-md:justify-center gap-8 md:px-6 py-3 md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-all duration-300 ${
+        className={`max-md:absolute max-md:top-0 max-md:w-full max-md:h-screen max-md:px-9 max-md:font-medium max-md:text-lg z-50 flex flex-col md:flex-row items-center max-md:justify-center gap-8 md:px-6 py-3 md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-all duration-300 ${
           isMenuOpen ? 'max-md:left-0' : 'max-md:left-full'
         }`}
       >
-
         <XIcon
           onClick={() => setIsMenuOpen(false)}
           className="md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer"
         />
 
-        <Link onClick={handleLinkClick} to="/">
-          Home
-        </Link>
-
-        <Link onClick={handleLinkClick} to="/movies">
-          Movies
-        </Link>
-
-        <Link onClick={handleLinkClick} to="/theaters">
-          Theaters
-        </Link>
-
-        <Link onClick={handleLinkClick} to="/releases">
-          Releases
-        </Link>
-
-        <Link onClick={handleLinkClick} to="/favorites">
-          Favorites
-        </Link>
+        <Link onClick={handleLinkClick} to="/">Home</Link>
+        <Link onClick={handleLinkClick} to="/movies">Movies</Link>
+        <Link onClick={handleLinkClick} to="/theaters">Theaters</Link>
+        <Link onClick={handleLinkClick} to="/releases">Releases</Link>
+        <Link onClick={handleLinkClick} to="/favorites">Favorites</Link>
 
       </div>
 
-      <div className="flex items-center gap-8">
+      <div className="flex items-center gap-5">
 
         <SearchIcon className="max-md:hidden w-6 h-6 cursor-pointer" />
 
-        <button className="px-4 py-1 sm:px-7 sm:py-2 bg-pink-600 hover:opacity-90 transition rounded-full font-medium cursor-pointer">
-          Login
-        </button>
+        {!user ? (
+          <button
+            onClick={() => openSignIn()}
+            className="px-4 py-1 sm:px-7 sm:py-2 bg-pink-600 hover:opacity-90 transition rounded-full font-medium cursor-pointer"
+          >
+            Login
+          </button>
+        ) : (
+          <UserButton afterSignOutUrl="/">
+            <UserButton.MenuItems>
+              <UserButton.Action
+                label="My Bookings"
+                labelIcon={<TicketPlus size={15} />}
+                onClick={() => navigate('/bookings')}
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        )}
 
       </div>
 
